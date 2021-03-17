0022bc4c8616afc508b787d3a7df23e3ebe010e3
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-17 14:39:57 +0100

    zadatak 6

---
+++ ex06/src/index.js
@@ -0,0 +1,13 @@
+var lion = {
+  //Only change code below this line
+  name: "Simba",
+  legs: 4,
+  tails: 1,
+};
+function myFunction(newparameter, secondparameter ) {
+  lion[newparameter] = secondparameter;
+  return lion;
+}
+myFunction("roar");
+console.log(myFunction("roar", "roar-roar"));
+module.exports = myFunction;
