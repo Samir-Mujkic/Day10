40faf1df862dad5bcec997f13e65d7681e918b15
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-17 16:13:22 +0100

    zadatak 7

--- ex07/src/index.js
+++ ex07/src/index.js
@@ -1,4 +1,4 @@
-function myFunction(roar) {
+function myFunction(propName) {
   //Only change code below this line
   var lion = {
     name: "Simba",
@@ -6,7 +6,7 @@ function myFunction(roar) {
     tails: 1,
     roar: "roar-roar",
   };
-  delete lion[roar];
+  delete lion[propName];
   return lion;
 }
 myFunction("roar");
