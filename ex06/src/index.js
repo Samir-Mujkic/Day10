154c5a8ca21c55c601dd34f4d8e622188affd0db
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-17 16:12:52 +0100

    zadatak 6

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -2,13 +2,13 @@ var lion = {
   //Only change code below this line
   name: "Simba",
   legs: 4,
-  tails: 1,
+  tails: 1
 };
-function myFunction(newparameter, secondparameter) {
-  lion[newparameter] = secondparameter;
+function myFunction(propName, propValue) {
+  lion[propName] = propValue;
   return lion;
 }
-myFunction("roar");
-console.log(myFunction("roar", "roar-roar"));
+myFunction("roar", "roar-roar");
+
 //Only change code above this line
 module.exports = myFunction;
