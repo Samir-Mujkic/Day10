d1d704d045561fbc11f3a75582e127522d6bd8e4
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-17 14:41:26 +0100

    zadatak 6

--- ex06/src/index.js
+++ ex06/src/index.js
@@ -4,10 +4,11 @@ var lion = {
   legs: 4,
   tails: 1,
 };
-function myFunction(newparameter, secondparameter ) {
+function myFunction(newparameter, secondparameter) {
   lion[newparameter] = secondparameter;
   return lion;
 }
 myFunction("roar");
 console.log(myFunction("roar", "roar-roar"));
+//Only change code above this line
 module.exports = myFunction;
