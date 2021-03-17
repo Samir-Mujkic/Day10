6916fc3abe0d99e2c8d3ce5bb262da5f46764c8a
Samir-Mujkic <samir.mujkic096@gmail.com>
2021-03-17 14:03:56 +0100

    zadatak 5

---
+++ ex05/src/index.js
@@ -0,0 +1,16 @@
+var school = {
+  //Only change code below this line
+  name: "Arena",
+  location: "Sarajevo",
+  established: "2020",
+};
+function myFunction(name) {
+  school.name = name;
+  return school;
+}
+console.log(myFunction("Paragon"));
+//Only change code above this line
+
+module.exports = { school, myFunction };
+
+
